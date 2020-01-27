ae4e07cbf41f6dee6d0e7fac7ec0fd858f4fc010
mehar <darakhshanararia@gmail.com>
2020-01-27 10:24:37 +0530

    all file added

---
+++ src/main/java/FirstPrgm.java
@@ -0,0 +1,11 @@
+public class FirstPrgm {
+    public static void main(String[] args) {
+        int temp,num=13456,a,rev=1;
+        while (num>0)
+            a=num;
+        temp=num%10;
+        rev=(rev*10)+temp;
+        num=num/10;
+        System.out.println(rev);
+    }
+}
